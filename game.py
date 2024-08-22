9216ecd55d5ef48060f079e7c9b5ccfb8f6da5df
k1m190r <148415+k1m190r@users.noreply.github.com>
2024-08-22 21:27:23 +0200

    basic circle works

--- game.py
+++ game.py
@@ -3,4 +3,20 @@ pygame.init()
 
 screen = pygame.display.set_mode([500, 500])
 
-print()
+running = True
+
+while running:
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    screen.fill((255, 255, 255)) # white
+
+    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+
+    pygame.display.flip()
+
+
+
+pygame.quit()
