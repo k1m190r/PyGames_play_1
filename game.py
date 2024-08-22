579562bc35e1921927dcdc274fcd17babc448fef
k1m190r <148415+k1m190r@users.noreply.github.com>
2024-08-22 22:12:38 +0200

    prior to enemies

--- game.py
+++ game.py
@@ -1,22 +1,67 @@
+import random
 import pygame
+
+from pygame.locals import K_UP, K_DOWN, K_LEFT, K_RIGHT, K_ESCAPE, KEYDOWN, QUIT, KEYUP
+
 pygame.init()
 
-screen = pygame.display.set_mode([500, 500])
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 
 running = True
 
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+
+    def update(self, pressed_keys):
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -5)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 5)
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-5, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(5, 0)
+
+        # limit to screen
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+
+
+player1 = Player()
+
 while running:
-    
     for event in pygame.event.get():
+        # Quitting
+        if event.type == KEYDOWN:
+            print(event.key)
+            if event.key == K_ESCAPE:
+                running = False
+
         if event.type == pygame.QUIT:
             running = False
-    
-    screen.fill((255, 255, 255)) # white
 
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+    pressed_keys = pygame.key.get_pressed()
+    player1.update(pressed_keys)
+
+    screen.fill((0, 0, 0))  # white
+
+    screen.blit(player1.surf, player1.rect)
 
     pygame.display.flip()
-
 
 
 pygame.quit()
